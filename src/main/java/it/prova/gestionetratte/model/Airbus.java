ab298ca1e56a2d5a2186629242223d6f7a58642e
RobertoEsposito02 <robiespo02@libero.it>
2022-11-25 15:04:28 +0100

    implementati model

---
+++ src/main/java/it/prova/gestionetratte/model/Airbus.java
@@ -0,0 +1,86 @@
+package it.prova.gestionetratte.model;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "airbus")
+public class Airbus {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "id")
+	private Long id;
+	@Column(name = "codice")
+	private String codice;
+	@Column(name = "descrizione")
+	private String descrizione;
+	@Column(name = "datainizioservizio")
+	private LocalDate dataInizioServizio;
+	@Column(name = "numeroPasseggeri")
+	private Integer numeroPassaggeri;
+	
+	@OneToMany(fetch = FetchType.LAZY, mappedBy = "tratta")
+	private List<Tratta> tratte = new ArrayList<>();
+	
+	public Airbus() {
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getCodice() {
+		return codice;
+	}
+
+	public void setCodice(String codice) {
+		this.codice = codice;
+	}
+
+	public String getDescrizione() {
+		return descrizione;
+	}
+
+	public void setDescrizione(String descrizione) {
+		this.descrizione = descrizione;
+	}
+
+	public LocalDate getDataInizioServizio() {
+		return dataInizioServizio;
+	}
+
+	public void setDataInizioServizio(LocalDate dataInizioServizio) {
+		this.dataInizioServizio = dataInizioServizio;
+	}
+
+	public Integer getNumeroPassaggeri() {
+		return numeroPassaggeri;
+	}
+
+	public void setNumeroPassaggeri(Integer numeroPassaggeri) {
+		this.numeroPassaggeri = numeroPassaggeri;
+	}
+
+	public List<Tratta> getTratte() {
+		return tratte;
+	}
+
+	public void setTratte(List<Tratta> tratte) {
+		this.tratte = tratte;
+	}
+	
+}
---
+++ src/main/java/it/prova/gestionetratte/model/Stato.java
@@ -0,0 +1,5 @@
+package it.prova.gestionetratte.model;
+
+public enum Stato {
+	ATTIVO, CONCLUSA, ANNULLATA;
+}
