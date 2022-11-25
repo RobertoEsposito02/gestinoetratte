638824ff705db264d04086aea54d888a4035df26
RobertoEsposito02 <robiespo02@libero.it>
2022-11-25 14:24:01 +0100

    creazione progetto

---
+++ src/main/java/it/prova/gestionetratte/GestionetratteApplication.java
@@ -0,0 +1,19 @@
+package it.prova.gestionetratte;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestionetratteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestionetratteApplication.class, args);
+	}
+	
+	
+	/*
+	ma che bella questa tastiera il layout è americano perché quando scrivo le e accentate va alla grande Balraj che cavolo stai guardando?
+	*/
+	
+	
+}
